40f5e44770212c8a57c753ab6641efc7a8944209
Benjamin Ikeh <b.ikeh@yahoo.com>
2023-02-13 16:08:13 +0000

    correct argument passing to train_test_split function

--- load_data.py
+++ load_data.py
@@ -6,7 +6,7 @@ import pennylane as qml
 import numpy as np
 
 iris = datasets.load_iris()
-x = iris['data']
+X = iris['data']
 y = iris['target']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
